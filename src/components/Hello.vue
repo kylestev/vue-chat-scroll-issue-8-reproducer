95fb682fcb17b7a90dd92e297069919441dd6b15
Kyle Stevenson <kyle@kylestevenson.me>
2017-04-17 21:40:10 -0700

    Reproducer

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="message_list" v-chat-scoll>
+      <div
+        v-for="message in messages"
+        v-text="message.body"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -25,7 +14,17 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      messages: [
+        { body: 'message #1' },
+        { body: 'message #2' },
+        { body: 'message #3' },
+        { body: 'message #4' },
+        { body: 'message #5' },
+        { body: 'message #6' },
+        { body: 'message #7' },
+        { body: 'message #8' },
+        { body: 'message #9' }
+      ]
     }
   }
 }
@@ -33,21 +32,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.message_list {
+  display: flex;
+  flex-direction: column;
+  max-height: 300px;
+  max-width: 300px;
+  overflow-y: scroll;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.message_list .message_list_item {
+  flex: 1;
+  padding: 5px;
 }
 </style>
